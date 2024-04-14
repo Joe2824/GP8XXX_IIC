1eaf4adb9165ba1d67a2f4e7eee4b152d47249f4
Joel Klein <joel_klein@me.com>
2024-04-14 21:43:51 +0200

    v0.0.4 version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="GP8XXX_IIC",
     keywords='Raspberry Pi, Raspi, GP8403, GP8503, GP8211S, GP8512, GP8413, GP8302, DAC',
-    version="0.0.3",
+    version="0.0.4",
     author="Joel Klein",
     description="The GP8XXX Python module offers an intuitive interface for controlling DAC (Digital to Analog Converter) devices via the I2C protocol. With support for various DAC models.",
     long_description=long_description,
